919bcbd06e3e26ffbeb1d61a9b3061c2c721b9f3
Rodrigo Prudente <rodrigo.prudente82@gmail.com>
2025-11-03 10:59:36 -0300

    first commit - base structure

---
+++ src/main_server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("Kessel server starting...\n");
+    printf("Build successful. Ready to implement core logic.\n");
+    return 0;
+}
